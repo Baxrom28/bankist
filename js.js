de930af10ae0ce0af58cc2719ba7bad82670bc55
Baxrom28 <usmonovbakhrom28@gmail.com>
2023-12-30 11:31:03 +0500

    Bankist

---
+++ js.js
@@ -0,0 +1,406 @@
+// // // random funksiyasi
+// // const firandom = (min, max) => Math.trunc(Math.random() * (max - min)+1) + min;
+// // console.log(firandom(10, 15));  // 10dan 15gacha 10 kirmidi 15 kiradi
+
+
+
+// // tugilgan kunim 
+// const tugilganKunim = new Date(2008, 1, 7);
+// console.log(tugilganKunim);
+// console.log(tugilganKunim.getDay());
+// // console.log(tugilganKunim);
+// // console.log(tugilganKunim.setFullYear(2008));
+
+"use strict";
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+/////////////////////////////////////////////////
+// Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
+const account1 = {
+    owner: "Jonas Schmedtmann",
+    movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+
+    movementsDates: [
+        "2019-11-18T21:31:17.178Z",
+        "2019-12-23T07:42:02.383Z",
+        "2020-01-28T09:15:04.904Z",
+        "2020-04-01T10:17:24.185Z",
+        "2020-05-08T14:11:59.604Z",
+        "2020-07-26T17:01:17.194Z",
+        "2020-07-28T23:36:17.929Z",
+        "2020-08-01T10:51:36.790Z",
+    ],
+    currency: "EUR",
+    locale: "pt-PT", // de-DE
+};
+
+const account2 = {
+    owner: "Jessica Davis",
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+
+    movementsDates: [
+        "2019-11-01T13:15:33.035Z",
+        "2019-11-30T09:48:16.867Z",
+        "2019-12-25T06:04:23.907Z",
+        "2020-01-25T14:18:46.235Z",
+        "2020-02-05T16:33:06.386Z",
+        "2020-04-10T14:43:26.374Z",
+        "2020-06-25T18:49:59.371Z",
+        "2020-07-26T12:01:20.894Z",
+    ],
+    currency: "USD",
+    locale: "en-US",
+};
+const account3 = {
+    owner: "Usmonov Baxrom",
+    movements: [5000, 400, 150, 790, 3210, 1000, 8500, 30],
+    interestRate: 0,
+    pin: 7777,
+    movementsDates: [
+        "2019-11-01T13:15:33.035Z",
+        "2019-11-30T09:48:16.867Z",
+        "2019-12-25T06:04:23.907Z",
+        "2020-01-25T14:18:46.235Z",
+        "2020-02-05T16:33:06.386Z",
+        "2020-04-10T14:43:26.374Z",
+        "2020-06-25T18:49:59.371Z",
+        "2020-07-26T12:01:20.894Z",
+    ],
+    currency: "UZS",
+    locale: "uz-UZ",
+};
+
+const accounts = [account1, account2, account3];
+
+/////////////////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
+
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
+
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
+
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
+
+/////////////////////////////////////////////////
+// Functions
+
+// pul chiqib turgan joyda datalar
+const formatMovementDate = function (date, locale) {
+    const calcDaysPassed = (date1, date2) =>
+        Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+    const daysPassed = calcDaysPassed(new Date(), date);
+    // console.log(daysPassed);
+
+    if (daysPassed === 0) return "Today";
+    if (daysPassed === 1) return "Yesterday";
+    if (daysPassed <= 7) return `${daysPassed} days ago`;
+    return new Intl.DateTimeFormat(locale).format(date);
+};
+
+
+// farmatlab berishi uchun pulni
+const formatCur = function (value, locale, currency) {
+    return new Intl.NumberFormat(locale, {
+        style: "currency",
+        currency: currency,
+    }).format(value);
+};
+
+const displayMovements = function (acc, sort = false) {
+    //boshidagi teglarni yoqotish uchun
+    containerMovements.innerHTML = "";
+    //pasroqda knopka bor oshani bosgantagi holat uchun
+    const movs = sort
+        ? acc.movements.slice().sort((a, b) => a - b)
+        : acc.movements;
+
+    movs.forEach(function (mov, i) {
+        const type = mov > 0 ? "deposit" : "withdrawal";
+        //- yoki + da ekanligi bilish uchun 
+        const date = new Date(acc.movementsDates[i]);
+        const displayDate = formatMovementDate(date, acc.locale);
+
+        const formattedMov = formatCur(mov, acc.locale, acc.currency);
+        //saytga chiqarish uchun shuni htmlga qoshamiz 
+        const html = `
+      <div class="movements__row" ${mov < 0 ? `style="background-color: rgba(255, 0, 0, 0.5);"` : `style="background-color: rgba(0, 128, 0, 0.5);"`
+            } >
+        <div class="movements__type movements__type--${type}">${i + 1
+            } ${type}</div>
+        <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${formattedMov}</div>
+      </div>
+    `;
+        //htmlga qoshish uchun
+        containerMovements.insertAdjacentHTML("afterbegin", html);
+    });
+};
+
+
+// opshi suma
+const calcDisplayBalance = function (acc) {
+    acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
+};
+
+// pasroqda ishlatilgan pul va kelgan pul bor oshani yozish 
+const calcDisplaySummary = function (acc) {
+    const incomes = acc.movements
+        .filter((mov) => mov > 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
+
+    const out = acc.movements
+        .filter((mov) => mov < 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
+
+    const interest = acc.movements
+        .filter((mov) => mov > 0)
+        .map((deposit) => (deposit * acc.interestRate) / 100)
+        .filter((int, i, arr) => {
+            // console.log(arr);
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
+};
+
+
+// username yarasish uchun
+const createUsernames = function (accs) {
+    accs.forEach(function (acc) {
+        acc.username = acc.owner
+            .toLowerCase()
+            .split(" ")
+            .map((name) => name[0])
+            .join("");
+    });
+};
+
+createUsernames(accounts);
+
+
+const updateUI = function (acc) {
+    // Display movements
+    displayMovements(acc);
+
+    // Display balance
+    calcDisplayBalance(acc);
+
+    // Display summary
+    calcDisplaySummary(acc);
+};
+// timer ------------------------
+const startLogOutTimer = function () {
+    const tick = function () {
+        const min = String(Math.trunc(time / 60)).padStart(2, 0);
+        const sec = String(time % 60).padStart(2, 0);
+
+        // In each call, print the remaining time to UI
+        labelTimer.textContent = `${min}:${sec}`;
+
+        // When 0 seconds, stop timer and log out user
+        if (time === 0) {
+            clearInterval(timer);
+            labelWelcome.textContent = "Log in to get started";
+            containerApp.style.opacity = 0;
+        }
+
+        // Decrease 1s
+        time--;
+    };
+
+    // Set time to 5 minutes
+    let time = 600;
+
+    // Call the timer every second
+    tick();
+    const timer = setInterval(tick, 1000);
+
+    return timer;
+};
+
+///////////////////////////////////////
+// Event handlers
+let currentAccount, timer;
+
+// FAKE ALWAYS LOGGED IN
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
+
+
+// user va pin kodni olish va kirish uchun
+btnLogin.addEventListener("click", function (e) {
+    // refresh qilmasligi uchun
+    e.preventDefault();
+
+
+    // inputdan malumot olib username bilan tekshitish uchun
+    currentAccount = accounts.find(
+        (accs) => accs.username === inputLoginUsername.value
+    );
+    console.log(currentAccount);
+
+
+    // agar pin kode teng bolsa osha accga kirishi uchun
+    if (currentAccount?.pin === +inputLoginPin.value) {
+        // acc kotinib salom berishi uchun
+        labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(" ")[0]
+            }  ${currentAccount.owner.split(" ")[1]}`;
+        containerApp.style.opacity = 100;
+
+        // vaqt va kun chiqishi uchun 
+        const now = new Date();
+        const options = {
+            hour: "numeric",
+            minute: "numeric",
+            day: "numeric",
+            month: "numeric",
+            year: "numeric",
+        };
+
+        labelDate.textContent = new Intl.DateTimeFormat(
+            currentAccount.locale,
+            options
+        ).format(now);
+
+        // inputni yozilgan narsani yoqotish uchun
+        inputLoginUsername.value = inputLoginPin.value = "";
+        inputLoginPin.blur();
+
+        // Timer
+        if (timer) clearInterval(timer);
+        timer = startLogOutTimer();
+
+        updateUI(currentAccount);
+    }
+});
+
+// pul otqizish uchun va uni qabul qilish uchun
+btnTransfer.addEventListener("click", function (e) {
+    e.preventDefault();
+    const amount = +inputTransferAmount.value;
+    // pul tushadigan odam receiverAcc
+    const receiverAcc = accounts.find(
+        (acc) => acc.username === inputTransferTo.value
+    );
+    inputTransferAmount.value = inputTransferTo.value = "";
+
+    if (
+        amount > 0 &&
+        receiverAcc &&
+        currentAccount.balance >= amount &&
+        receiverAcc?.username !== currentAccount.username
+    ) {
+        // olib tashlash uchun
+        currentAccount.movements.push(-amount);
+        receiverAcc.movements.push(amount);
+
+        // otqizilgan datasi  
+        currentAccount.movementsDates.push(new Date().toISOString());
+        receiverAcc.movementsDates.push(new Date().toISOString());
+
+        updateUI(currentAccount);
+
+        // timer
+        clearInterval(timer);
+        timer = startLogOutTimer();
+    }
+});
+
+// kridit olish uchun
+btnLoan.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const amount = Math.floor(inputLoanAmount.value);
+
+    if (
+        amount > 0 &&
+        currentAccount.movements.some((mov) => mov >= amount * 0.1)
+    ) {
+        setTimeout(function () {
+            // pulni qoshish
+            currentAccount.movements.push(amount);
+
+            // datesi
+            currentAccount.movementsDates.push(new Date().toISOString());
+
+            updateUI(currentAccount);
+
+            // timer yani 2.5 sikunt dan keyin berishi uchun
+            clearInterval(timer);
+            timer = startLogOutTimer();
+        }, 10000);
+    }
+    inputLoanAmount.value = "";
+});
+
+
+// chiqib ketishi uchun
+btnClose.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (
+        inputCloseUsername.value === currentAccount.username &&
+        +inputClosePin.value === currentAccount.pin
+    ) {
+        // const index = accounts.findIndex(
+        //     (acc) => acc.username === currentAccount.username
+        // );
+        // console.log(index);
+        // .indexOf(23)
+
+        // Delete account
+        // accounts.splice(index, 1);
+
+        containerApp.style.opacity = 0;
+        labelWelcome.textContent = `Log in to get started`
+    }
+
+    inputCloseUsername.value = inputClosePin.value = "";
+});
+
+
+// knopga true qaytarishi uchun 
+
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+    e.preventDefault();
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
